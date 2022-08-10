0430b86efa6d4b7654fa4594f10a25513c9d6d98
Deshant Sachdeva <deshant0099@gmail.com>
2022-08-10 11:00:41 -0600

    Initial commit

---
+++ src/main/java/com/usermanagement/app/UserManagementAndAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.usermanagement.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementAndAuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementAndAuthenticationAppApplication.class, args);
+	}
+
+}
